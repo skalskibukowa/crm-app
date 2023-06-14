72b8e5d89574b114d75254d0f8b443a6503ea74a
Bartosz Markiewicz <bartosz.markiewicz01@gmail.com>
2023-06-14 20:34:13 +0200

    ProjectSchema has been created

--- Backend/models/user.models.js
+++ Backend/models/user.models.js
@@ -10,7 +10,8 @@ const userSchema = new Schema(
         email: { type: String, required: true, unique: true},
         password: { type: String, required: true, minLength: 6},
         jobTitle: { type: String, required: true},
-        image: {type: String, required: false}
+        image: {type: String, required: false},
+        AssignedProject: { type: mongoose.Types.ObjectId, required: true, ref: 'Project'}
     },
     {timestamps: true}
 );
