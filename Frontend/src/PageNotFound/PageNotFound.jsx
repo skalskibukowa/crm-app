d4ae0d97d5348f60cebab4c073fa8865e7a87595
skalskibukowa <bartosz.markiewicz01@gmail.com>
2023-08-04 14:54:01 +0200

    PageNotFound page has been created. Token logic has been applied - without generated token end-user is only able to access login / register page.

---
+++ Frontend/src/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <h1 className="text-red-600 h-56 grid content-center justify-center">PageNotFound</h1>
+  )
+}
+
+export default PageNotFound;
