acf1d58efccca122e2309713182c9d12c4b25d5e
skalskibukowa <bartosz.markiewicz01@gmail.com>
2023-08-09 21:39:03 +0200

    Toggle sidebar function has been added

--- Frontend/src/Components/SidebarNav.jsx
+++ Frontend/src/Components/SidebarNav.jsx
@@ -1,18 +1,27 @@
 import React, {useState} from 'react';
 import SidebarItem from './SidebarItem';
+import LogoutButton from './LogoutButton';
 
 
 const SidebarNav = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
+    const toggleSidebar = () => {
+      setIsSidebarOpen(!isSidebarOpen);
+    };
+
   return (
     <div className="flex h-screen">
+      {isSidebarOpen && (
       <div
-        className={`bg-gray-800 text-white sidebar w-96 ${isSidebarOpen ? '' : 'closed'}`}
+        className="bg-gray-800 text-white sidebar w-96"
       >
+        
         <div className="p-4">
+        <button onClick={toggleSidebar}>Toogle Bar</button>
           <h1 className="text-2xl font-semibold">Sidebar</h1>
         </div>
+        
         <nav className="p-4">
           <ul>
             <li className="mb-2">
@@ -49,7 +58,26 @@ const SidebarNav = () => {
             </SidebarItem>
           </ul>
         </nav>
+        <div className="absolute bottom-4 left-4">
+          <LogoutButton />
+        </div>
+        
       </div>
+      )}
+
+
+      {!isSidebarOpen && (
+        <>
+          <div className="absolute top-4 left-4">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded border-spacing-1" onClick={toggleSidebar}>
+                Open SideBar
+              </button>
+            </div>
+            <div className="absolute bottom-4 left-4">
+                <LogoutButton />
+            </div>
+          </>
+      )}
       
       <div className="flex-1 p-4">
         {/* Main content goes here */}
